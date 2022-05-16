86b12a683abf832b5ea4dec1a3d3eefcee8d2c1d
stephenlambertfs <97977357+stephenlambertfs@users.noreply.github.com>
2022-05-16 14:02:44 -0600

    Adding JS File

---
+++ basics.js
@@ -0,0 +1,16 @@
+//Write a program that creates a numeric, a string, and a boolean variable
+// Make sure to use let, var, and const 
+
+
+
+//Using the varaibles above, display the data type of each variable
+//For example: console.log("number is a", typeof, "data type")
+
+
+//Using the variables below, create a if/else statement to find  and display the letter grade
+//Your statements should check for each letter grade i.e., A,B,C,D,and F
+let grade = 90
+
+//Using the variables below, create a swtich statement to find and display the day of the week
+let day = 2
+let day_name = "Monday"
